34edc54e3e144069770961b74aab2f5926f1aca2
Yaqzan <yali6@uwo.ca>
2013-11-03 15:37:08 -0500

    Stats page done

--- src/gui/StatsMenu.java
+++ src/gui/StatsMenu.java
@@ -18,8 +18,8 @@ public class StatsMenu extends BackgroundPanel {
 		
 		super("http://jbaron6.cs2212.ca/img/default_background.png", new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-	
-// Add the logo		
+		//-------------------------------------------------------------------------------------------------		
+		// Add the logo		
 		c.fill = GridBagConstraints.BOTH;
 		c.gridwidth = GridBagConstraints.CENTER;
 		c.insets = new Insets(10,150,10,150);
@@ -34,21 +34,51 @@ public class StatsMenu extends BackgroundPanel {
 			add(new JLabel("Stats & Scores"), c);
 		}
 
-//-------------------------------------------------------------------------------------------------		
-		c.insets = new Insets(5,5,5,5);
+		//-------------------------------------------------------------------------------------------------		
+		//Tabs
+		JTabbedPane tabs = new JTabbedPane();
+		JPanel tab1 = new JPanel();
+		JPanel tab2 = new JPanel();
+		JPanel tab3 = new JPanel();
+		tab1.setLayout(new GridBagLayout());
+		fillTabFriends(tab1, c);
+		fillTabAge(tab2, c);
+		fillTabLevels(tab3, c);
+		
+		// Add the tabs
+		tabs.addTab("Friends", tab1);
+		tabs.addTab("By Age", tab2);
+		tabs.addTab("By Level", tab3);
+		c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.gridwidth = GridBagConstraints.CENTER;
+		c.insets = new Insets(10,150,10,150);
+		c.weightx = 0;
+		c.gridx = 0;
+		c.gridy = 1;
+		add(tabs, c);
+
+
+		
+	}
+	private void fillTabFriends(JPanel tab1, GridBagConstraints c){
+		// Headers	
+		
+		c.insets = new Insets(5,5,5,10);
 		c.gridx = 0;
 		c.gridwidth = 2;
 		c.gridheight =1;
-		c.gridy = 1;
-		add(new JLabel("Friends"), c);
+		c.gridy = 0;
+		tab1.add(new JLabel("Friends"), c);
 		c.gridx = 2;
 		c.gridwidth = 1;
-		add(new JLabel("Children"), c);
+		tab1.add(new JLabel("Children"), c);
 		c.gridx = 3;
-		add(new JLabel("Age"), c);
+		tab1.add(new JLabel("Age"), c);
 		c.gridx = 4;
-		add(new JLabel("Level"), c);
-		
+		tab1.add(new JLabel("Level"), c);
+		//-------------------------------------------------------------------------------------------------		
+		// Profile Pictures
 		c.gridx = 0;
 		c.gridy=2;
 		c.gridheight =4;	
@@ -57,84 +87,186 @@ public class StatsMenu extends BackgroundPanel {
 			try	{
 				Image img = ImageIO.read(new URL("http://jbaron6.cs2212.ca/img/profilePictures/"+i+".jpg"));
 				JLabel pic = new JLabel(new ImageIcon(getScaledImage(img, 55,55)));
-				add(pic, c);
+				tab1.add(pic, c);
 			} catch (IOException e) {
-				add(new JLabel("Profile Picture"), c);
+				tab1.add(new JLabel("Profile Picture"), c);
 			}
-			c.gridy=(i*4)+2;
+			c.gridy=(i*5)+2;
 		}
-//-------------------------------------------------------------------------------------------------			
-		
+
+		//-------------------------------------------------------------------------------------------------	
+		// Friends names
 		c.gridy = 2;
 		c.gridx= 1;
-		add(new JLabel("James A."), c);
-		c.gridy+=4;
-		add(new JLabel("Yaqzan"), c);
-		c.gridy+=4;
-		add(new JLabel("Chuhan"), c);
-		c.gridy+=4;
-		add(new JLabel("Taylor"), c);
-		c.gridy+=4;
-		add(new JLabel("James B."), c);
+		c.gridwidth =1;
+		tab1.add(new JLabel("James Anderson"), c);
+		c.gridy+=5;
+		tab1.add(new JLabel("Yaqzan Ali"), c);
+		c.gridy+=5;
+		tab1.add(new JLabel("Chuhan Frank"), c);
+		c.gridy+=5;
+		tab1.add(new JLabel("Taylor Joseph"), c);
+		c.gridy+=5;
+		tab1.add(new JLabel("James Baron"), c);
 
-//-------------------------------------------------------------------------------------------------		
-		c.insets = new Insets(0,0,0,0);
+		//-------------------------------------------------------------------------------------------------		
+		// Children Names
+		c.insets = new Insets(0,10,0,0);
 		c.gridy = 2;
 		c.gridx = 2;
 		c.gridheight = 1;
-		add(new JLabel("Jim"), c);
+		tab1.add(new JLabel("Lisa"), c);
 		c.gridy++;
-		add(new JLabel("Pam"), c);
-		c.gridy++;
-		add(new JLabel("Michael"), c);
-		c.gridy++;
-		add(new JLabel("Dwight"), c);
-		c.gridy++;
-		add(new JLabel("Troy"), c);
-		c.gridy++;
-		add(new JLabel("Abed"), c);
-		c.gridy++;
-		add(new JLabel("Britta"), c);
-		c.gridy++;
-		add(new JLabel("Annie"), c);
-		c.gridy++;
-		add(new JLabel("Sheldon"), c);
+		tab1.add(new JLabel("Bart"), c);
 		c.gridy+=4;
-		add(new JLabel("Leslie"), c);
+		
+		tab1.add(new JLabel("Troy"), c);
 		c.gridy++;
-		add(new JLabel("Ron"), c);
+		tab1.add(new JLabel("Abed"), c);
+		c.gridy++;
+		tab1.add(new JLabel("Britta"), c);
 		c.gridy+=3;
-		add(new JLabel("Lisa"), c);
-		c.gridy++;
-		add(new JLabel("Bart"), c);
-		c.gridy++;
 
+		tab1.add(new JLabel("Sheldon"), c);
+		c.gridy+=5;
+		tab1.add(new JLabel("Leslie"), c);
+		c.gridy++;
+		tab1.add(new JLabel("Ron"), c);
+		c.gridy+=4;
+		tab1.add(new JLabel("Dwight"), c);
+		c.gridy++;
+		tab1.add(new JLabel("Jim"), c);
+		c.gridy++;
+		tab1.add(new JLabel("Michael"), c);
+		c.gridy++;
+		tab1.add(new JLabel("Pam"), c);
 		
-//-------------------------------------------------------------------------------------------------	
+		
+		//-------------------------------------------------------------------------------------------------	
+		// Age
+		c.insets = new Insets(0,10,0,10);
 		c.gridx = 3;
-		for(int i=2; i< 22;i++){
-			c.gridy= i;
-			int x = 3 + (int)(Math.random() * ((20 - 3) + 1));
-			add(new JLabel(Integer.toString(x)), c);
-		}
+		c.gridy = 2;
 		
-//-------------------------------------------------------------------------------------------------		
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy+=4;
+		
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy+=3;
+
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy+=5;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy+=4;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		c.gridy++;
+		tab1.add(new JLabel((Integer.toString(3 + (int)(Math.random() * ((20 - 3) + 1))))+" years old"), c);
+		
+		//-------------------------------------------------------------------------------------------------		
+		// Level
 		c.gridx = 4;
-		for(int i=2; i< 22;i++){
-			c.gridy= i;
-			int x = 1 + (int)(Math.random() * 12);
-			add(new JLabel(Integer.toString(x)), c);
-			
+		c.gridy = 2;
+		
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy+=4;
+		
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy+=3;
+
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy+=5;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy+=4;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		c.gridy++;
+		tab1.add(new JLabel(("Level "+Integer.toString(1 + (int)(Math.random() * 12)))), c);
+		//-------------------------------------------------------------------------------------------------		
+		// Separators
+		c.insets = new Insets(0,0,0,0);
+		//c.fill = GridBagConstraints.HORIZONTAL;
+		//c.weightx = 1;
+	    c.gridwidth = 5;
+		c.gridheight =1;
+		c.gridx = 0;
+		c.gridy = 1;
+		for(int i = 1; i< 6;i++){
+			JSeparator sep = new JSeparator(JSeparator.HORIZONTAL);
+			sep.setPreferredSize(new Dimension(1,30));
+
+			tab1.add(sep,c);
+					
+			c.gridy+=5;
 		}
-//-------------------------------------------------------------------------------------------------			
-
-
-
-//-------------------------------------------------------------------------------------------------		
-
+			
 	}
 
+	private void fillTabLevels(JPanel tab, GridBagConstraints c){
+		c = new GridBagConstraints();
+		c.insets = new Insets(0,0,0,0);
+		c.gridwidth = 1;
+		c.gridheight =1;
+		c.gridx = 1;
+		c.gridy = 0;
+		tab.add(new JLabel("Level "),c);
+		
+        //-------------------------------------------------------------------------------------------------	
+		//Level Combo Box
+		Vector<String> m = new Vector<String>();
+		m.add("1");
+		m.add("2");
+		m.add("3");
+		m.add("4");
+		m.add("5");
+		m.add("6");
+		m.add("7");
+		m.add("8");
+		m.add("9");
+		m.add("10");
+		m.add("11");
+		m.add("12");
+		JComboBox<String> levels = new JComboBox<String>(m);
+		c.gridx = 2;
+		tab.add(levels, c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		for(int i =1;i<4;i++){
+			c.gridy+=2;
+			tab.add(new JLabel(Integer.toString(i)), c);
+		}
+		
+		
+	}
+	
+	private void fillTabAge(JPanel tab, GridBagConstraints c){
 
+		
+	}
+// Resize ImageIcon
 	private Image getScaledImage(Image srcImg, int w, int h){
 	    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 	    Graphics2D g2 = resizedImg.createGraphics();
