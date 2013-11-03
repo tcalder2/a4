658590adbbd0cedd63a6ed03934b1eb8b09bec36
Yaqzan <yali6@uwo.ca>
2013-11-03 01:17:55 -0500

    Updated Stats Page

--- src/gui/StatsMenu.java
+++ src/gui/StatsMenu.java
@@ -18,6 +18,8 @@ public class StatsMenu extends BackgroundPanel {
 		
 		super("http://jbaron6.cs2212.ca/img/default_background.png", new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
+	
+// Add the logo		
 		c.fill = GridBagConstraints.BOTH;
 		c.gridwidth = GridBagConstraints.CENTER;
 		c.insets = new Insets(10,150,10,150);
@@ -33,53 +35,103 @@ public class StatsMenu extends BackgroundPanel {
 		}
 
 //-------------------------------------------------------------------------------------------------		
-		c.insets = new Insets(0,75,0,75);
-		c.gridy = 1;
+		c.insets = new Insets(5,5,5,5);
 		c.gridx = 0;
-		c.ipady = 300;
-		c.ipadx = 150;
-
-		
-		JTabbedPane tabs = new JTabbedPane();
-		JPanel tabFriends = new JPanel();
-		JPanel tabScores = new JPanel();
-		
-		tabs.addTab("Friends", tabFriends);
-		tabs.addTab("High SCores", tabScores);
-		add(tabs, c);
-//-------------------------------------------------------------------------------------------------			
-
-		tabFriends.setLayout(new GridBagLayout());
-		c.insets = new Insets(0,20,0,20);
+		c.gridwidth = 2;
+		c.gridheight =1;
 		c.gridy = 1;
-		c.ipadx = 0;
-		c.ipady = 0;
-		c.fill = GridBagConstraints.NONE;
+		add(new JLabel("Friends"), c);
+		c.gridx = 2;
 		c.gridwidth = 1;
-		c.gridheight =4;
-		c.anchor = GridBagConstraints.FIRST_LINE_START;
-
+		add(new JLabel("Children"), c);
+		c.gridx = 3;
+		add(new JLabel("Age"), c);
+		c.gridx = 4;
+		add(new JLabel("Level"), c);
+		
+		c.gridx = 0;
+		c.gridy=2;
+		c.gridheight =4;	
 		for(int i=1; i<6;i++){
-			c.gridy+=4;
+			
 			try	{
 				Image img = ImageIO.read(new URL("http://jbaron6.cs2212.ca/img/profilePictures/"+i+".jpg"));
 				JLabel pic = new JLabel(new ImageIcon(getScaledImage(img, 55,55)));
-				tabFriends.add(pic, c);
+				add(pic, c);
 			} catch (IOException e) {
-				tabFriends.add(new JLabel("Profile Picture"), c);
+				add(new JLabel("Profile Picture"), c);
 			}
+			c.gridy=(i*4)+2;
 		}
-
-		c.gridy = 0;
+//-------------------------------------------------------------------------------------------------			
+		
+		c.gridy = 2;
 		c.gridx= 1;
-		tabFriends.add(new JLabel("James"), c);
-		c.gridy = 0;
-		c.gridx= 2;
+		add(new JLabel("James A."), c);
+		c.gridy+=4;
+		add(new JLabel("Yaqzan"), c);
+		c.gridy+=4;
+		add(new JLabel("Chuhan"), c);
+		c.gridy+=4;
+		add(new JLabel("Taylor"), c);
+		c.gridy+=4;
+		add(new JLabel("James B."), c);
+
+//-------------------------------------------------------------------------------------------------		
+		c.insets = new Insets(0,0,0,0);
+		c.gridy = 2;
+		c.gridx = 2;
 		c.gridheight = 1;
-		tabFriends.add(new JLabel("Jim"), c);
-		c.gridy = 1;
-		c.gridx= 2;
-		tabFriends.add(new JLabel("Dwight"), c);
+		add(new JLabel("Jim"), c);
+		c.gridy++;
+		add(new JLabel("Pam"), c);
+		c.gridy++;
+		add(new JLabel("Michael"), c);
+		c.gridy++;
+		add(new JLabel("Dwight"), c);
+		c.gridy++;
+		add(new JLabel("Troy"), c);
+		c.gridy++;
+		add(new JLabel("Abed"), c);
+		c.gridy++;
+		add(new JLabel("Britta"), c);
+		c.gridy++;
+		add(new JLabel("Annie"), c);
+		c.gridy++;
+		add(new JLabel("Sheldon"), c);
+		c.gridy+=4;
+		add(new JLabel("Leslie"), c);
+		c.gridy++;
+		add(new JLabel("Ron"), c);
+		c.gridy+=3;
+		add(new JLabel("Lisa"), c);
+		c.gridy++;
+		add(new JLabel("Bart"), c);
+		c.gridy++;
+
+		
+//-------------------------------------------------------------------------------------------------	
+		c.gridx = 3;
+		for(int i=2; i< 22;i++){
+			c.gridy= i;
+			int x = 3 + (int)(Math.random() * ((20 - 3) + 1));
+			add(new JLabel(Integer.toString(x)), c);
+		}
+		
+//-------------------------------------------------------------------------------------------------		
+		c.gridx = 4;
+		for(int i=2; i< 22;i++){
+			c.gridy= i;
+			int x = 1 + (int)(Math.random() * 12);
+			add(new JLabel(Integer.toString(x)), c);
+			
+		}
+//-------------------------------------------------------------------------------------------------			
+
+
+
+//-------------------------------------------------------------------------------------------------		
+
 	}
 
 
