0fa88a08bcd53da159639f48a233897e7e70e92a
james <jbaron6@uwo.ca>
2013-11-10 21:24:59 -0500

    test

--- module/User/src/User/Controller/UserController.php
+++ module/User/src/User/Controller/UserController.php
@@ -146,6 +146,7 @@ class UserController extends AbstractActionController
  public function SetPasswordAction()
  {
   //blah
+  //blah
  	
   $user = $this->getUserTable()->getUser();
   $old_password = $this->params()->fromQuery('old_password');
