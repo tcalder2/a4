765e48497f1b6e7a0666d65ea27cfc6ad3d41356
jamesb <jamesb@ec>
2013-11-19 02:07:02 -0500

    getProgenies now working

--- test/src/service/ProgenyServiceTest.java
+++ test/src/service/ProgenyServiceTest.java
@@ -3,6 +3,7 @@ package service;
 import static org.junit.Assert.*;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 
 import json.JSONFailureException;
@@ -25,7 +26,53 @@ public class ProgenyServiceTest {
 
 	@Test
 	public void testGetProgenies() {
-		fail("Not yet implemented");
+	
+		Date birthDate = null;
+		int fiveMinutes = 3000;
+		
+		try {
+			birthDate = ProgenyService.getBirthDateFormat().parse("1982-05-19");
+		} catch (ParseException e1) {
+		}
+		
+		try {
+			ProgenyService.addProgeny("James", birthDate, fiveMinutes);
+		} catch (JSONFailureException e) {
+			fail(e.getMessages().get(0));
+		}
+
+		try {
+			ProgenyService.addProgeny("Frank", birthDate, fiveMinutes);
+		} catch (JSONFailureException e) {
+			fail(e.getMessages().get(0));
+		}
+		
+
+		try {
+			ProgenyService.addProgeny("Taylor", birthDate, fiveMinutes);
+		} catch (JSONFailureException e) {
+			fail(e.getMessages().get(0));
+		}
+		
+		ArrayList<Progeny> progenies = null;
+		
+		try {
+			progenies = ProgenyService.getProgenies();
+		} catch (JSONFailureException e) {
+			fail(e.getMessages().get(0));
+		}
+		
+		assertEquals("James", progenies.get(progenies.size() - 3).getFirstName());
+		assertEquals("Frank", progenies.get(progenies.size() - 2).getFirstName());
+		assertEquals("Taylor", progenies.get(progenies.size() - 1).getFirstName());
+		
+		try {
+			ProgenyService.removeProgeny(progenies.get(progenies.size() - 3));
+			ProgenyService.removeProgeny(progenies.get(progenies.size() - 2));
+			ProgenyService.removeProgeny(progenies.get(progenies.size() - 1));
+		} catch (JSONFailureException e) {
+			fail(e.getMessages().get(0));
+		}
 	}
 
 	@Test
@@ -39,7 +86,7 @@ public class ProgenyServiceTest {
 		} catch (ParseException e1) {
 		}
 		
-		String name = "James";
+		String name = "Jeff";
 		Progeny progeny = null;
 		
 		try {
@@ -71,7 +118,7 @@ public class ProgenyServiceTest {
 		} catch (ParseException e1) {
 		}
 		
-		String name = "James";
+		String name = "Bob";
 		Progeny progeny = null;
 		
 		try {
