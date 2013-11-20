f8d05f0e237781a77c5e9dd511dc20de8275b921
jamesb <jamesb@ec>
2013-11-20 16:02:12 -0500

    updating level module

--- module/LevelProgeny/config/module.config.php
+++ module/LevelProgeny/config/module.config.php
@@ -1,3 +1,51 @@
 <?php
 return array(
+ 'doctrine' => array(
+  'driver' => array(
+   'level_progeny_entities' => array(
+    'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+    'cache' => 'array',
+    'paths' => array(__DIR__ . '/../src/LevelProgeny/Entity')
+   ),
+
+   'orm_default' => array(
+    'drivers' => array(
+     'LevelProgeny\Entity' => 'level_progeny_entities'
+    )
+   ))),
+
+ 'router' => array(
+  'routes' => array(
+
+   'changemistakesallowed' => array(
+    'type' => 'Zend\Mvc\Router\Http\Literal',
+    'options' => array(
+     'route' => '/changelevelprogenymistakesallowed',
+     'defaults' => array(
+      'controller' => 'Levelprogeny\Controller\Levelprogeny',
+      'action' => 'changemistakesallowed',
+     ),
+    ),
+   ),
+
+
+  ),
+ ),
+
+ 'view_manager' => array(
+  'template_path_stack' => array(
+   'page' => __DIR__ . '/../view',
+  ),
+  'strategies' => array(
+   'ViewJsonStrategy',
+  ),
+ ),
+
+ 'controllers' => array(
+  'invokables' => array(
+   'LevelProgeny\Controller\LevelProgeny' => 'LevelProgeny\Controller\LevelProgenyController'
+  ),
+ ),
+
+
 );
