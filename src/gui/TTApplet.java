34f59889bac96ddce56d75e1960a702bad7c2ce9
jande2 <jande2@uwo.ca>
2013-11-22 19:00:25 -0500

    Trying to fix some git error I caused merging things

---
+++ src/gui/TTApplet.java
@@ -0,0 +1,54 @@
+package gui;
+
+import java.awt.Color;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import json.JSONFailureException;
+
+/**
+ * The class TTApplet, a JApplet.
+ * 
+ * @author James Anderson
+ * @version 1.6
+ */
+@SuppressWarnings("serial")
+public class TTApplet extends JApplet {
+		
+	/* (non-Javadoc)
+	 * @see java.applet.Applet#init()
+	 */
+	@Override
+	public void init() {
+		GeneralDialogue gd = new GeneralDialogue(null, null);
+		gd.setVisible(true);
+		try {
+			//Sets up the GUI controller
+			Controller.initialise();
+			
+			//Create the title panel and add to the view
+			TitlePanel titlePanel = new TitlePanel();
+			Controller.setBanner(titlePanel);
+			
+			//Create the welcome screen and add to the view
+			Welcome welcome = new Welcome();
+			Controller.setScreen(welcome);
+			
+			//Set the main split pane frame colour
+			Controller.setFrameColour(Color.BLACK);
+			
+			//Add the split pane to the applet view
+			this.add(Controller.getPane());
+		
+		//If there is a communication error display a panel with an error message
+		} catch (JSONFailureException e) {
+			JPanel screen = new JPanel();
+			screen.add(new JLabel("<html>Oops!<br>"
+					+ "It appears we are having some issues!<br>"
+					+ "Make sure you are logged into Facebook and try again.</html>"));
+			this.add(screen);
+		}
+	}
+}
