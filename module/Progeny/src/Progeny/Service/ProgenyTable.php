065ca7586b6c1ef101b521a3f77035ebed78245b
jamesb <jamesb@jbaron6.cs2212.ca>
2013-11-26 00:09:14 -0500

    Working on Progeny

--- module/Progeny/src/Progeny/Service/ProgenyTable.php
+++ module/Progeny/src/Progeny/Service/ProgenyTable.php
@@ -51,7 +51,7 @@ class ProgenyTable
  public function newProgeny($data)
  {
   /** @var \LevelProgeny\Service\LevelProgenyTable $level_progeny_table */
-  $level_progeny_table = $this->sm->get('Progeny\Service\ProgenyTable');
+  $level_progeny_table = $this->sm->get('Progeny\Service\LevelProgenyTable');
 
   $progeny = new Progeny();
 
